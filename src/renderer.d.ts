fddc49d49bc08f267f322064bbcc85b868ffccb7
Kyle Upton <kyleaupton@gmail.com>
2023-09-25 18:57:40 -0400

    removed ipc from types

--- src/renderer.d.ts
+++ src/renderer.d.ts
@@ -1,9 +1,7 @@
 import * as api from '../electron/main/api';
-import ipc from '../electron/main/api/ipc';
 
 declare global {
   interface Window {
     api: typeof api;
-    ipc: typeof ipc;
   }
 }
