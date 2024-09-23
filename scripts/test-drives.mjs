22c5f3606f776d9bfc211b857a58377356fb6715
Kyle Upton <kyleaupton@gmail.com>
2024-09-23 19:26:00 -0400

    wip: test drive script

--- scripts/test-drives.mjs
+++ scripts/test-drives.mjs
@@ -3,9 +3,9 @@ import child_process from 'node:child_process';
 import path from 'node:path';
 import url from 'node:url';
 import util from 'node:util';
-import { program } from 'commander';
 import inquirer from 'inquirer';
 import ora from 'ora';
+import { table } from 'table';
 
 const TEST_DRIVE_DIR = 'test-drives';
 const exec = util.promisify(child_process.exec);
@@ -15,102 +15,198 @@ const __project = path.join(__dirname, '..');
 const __testDrives = path.join(__project, TEST_DRIVE_DIR);
 
 /**
- * @returns {Promise<string[]>}
+ * Represents a drive
+ * @typedef {{
+ *  name: string;
+ *  mounted: boolean;
+ *  size: string;
+ * }} Drive
+ *
+ * Represents mount info
+ * @typedef {{
+ *  name: string;
+ *  imagePath: string;
+ *  mountPath: string;
+ * }} MountInfo
  */
-const getDrives = async () => {
-  return new Promise((resolve, reject) => {
-    fs.readdir(__testDrives)
-      .then(resolve)
-      .catch((error) => {
-        if (error instanceof Error && error.code === 'ENOENT') {
-          resolve([]);
-        } else {
-          reject(error);
-        }
+
+/**
+ * getMountInfo
+ * @returns {Promise<MountInfo[]>}
+ */
+const getMountInfo = async () => {
+  /** @type {MountInfo[]} */
+  const payload = [];
+  const imagePathRegex = /image-path\s+:\s+(.+)/;
+  const mountedPathRegex = /\/Volumes\/[^\s]+/;
+
+  const { stdout: mountStdout } = await exec('hdiutil info');
+
+  // Each section is separated by a line of equal signs
+  // As to not hard-code the number of equal signs, we use a regex
+  // I assume there will be at least 5 equal signs
+  const sections = mountStdout.split(/={5,}/);
+
+  for (const section of sections) {
+    const imagePathMatch = section.match(imagePathRegex);
+    const mountedPathMatch = section.match(mountedPathRegex);
+
+    if (imagePathMatch) {
+      const imagePath = imagePathMatch[1];
+
+      payload.push({
+        name: path.basename(imagePath),
+        imagePath,
+        mountedPath: mountedPathMatch ? mountedPathMatch[0] : null,
       });
-  });
+    }
+  }
+
+  return payload;
 };
 
-program
-  .command('list')
-  .description('List all test drives')
-  .action(async () => {
-    const drives = await getDrives();
+/**
+ * @returns {Promise<Drive[]>}
+ */
+const getDrives = async () => {
+  /** @type {Drive[]} */
+  const payload = [];
 
-    if (drives.length === 0) {
-      console.log('No test drives found');
-      return;
-    }
+  const mountInfo = await getMountInfo();
 
-    console.log('Test Drives:');
-    drives.forEach((drive) => {
-      console.log(`- ${drive}`);
-    });
-  });
+  /** @type {string[]} */
+  let existingDrives = [];
+  try {
+    existingDrives = await fs.readdir(__testDrives);
+  } catch (e) {
+    // Noop
+  }
 
-program
-  .command('create')
-  .option('-s, --size <size>', 'size of the test drive')
-  .option('-n, --name <name>', 'name of the test drive')
-  .description('Create a test drive')
-  .action(async ({ size = '8', name } = {}) => {
-    if (!name) {
-      const drives = await getDrives();
-      const baseName = 'test-drive';
-
-      const getName = (n = 1) => {
-        const driveName = `${baseName}-${n}`;
-        if (drives.find((x) => x.startsWith(driveName))) {
-          return getName(n + 1);
-        }
-        return driveName;
-      };
-
-      name = getName();
-    }
-
-    const _name = `${name}.dmg`;
-
-    const size_GB = Math.min(Math.max(parseInt(size), 1), 12);
-    const size_MB = size_GB * 1024;
-
-    const spinner = ora(
-      `Creating a test drive called ${_name} with ${size_GB}G of storage`,
-    ).start();
-
-    // Ensure the test drives directory exists
-    await fs.mkdir(__testDrives, { recursive: true });
-
-    await exec(
-      `hdiutil create -size ${size_MB}m -fs MS-DOS -volname "name" ${path.join(
-        __testDrives,
-        _name,
-      )}`,
+  for (const existingDrive of existingDrives) {
+    // Get size of drive
+    const { stdout: sizeStdout } = await exec(
+      `du -sh ${path.join(__testDrives, existingDrive)}`,
     );
 
-    spinner.succeed('Test drive created');
-  });
+    payload.push({
+      name: existingDrive,
+      mounted: !!mountInfo.find((x) => x.name === existingDrive),
+      size: sizeStdout.split('\t')[0],
+    });
+  }
 
-program
-  .command('mount')
-  .description('Mount a test drive')
-  .action(async () => {
-    const drives = await getDrives();
+  return payload;
+};
 
-    const { drive } = await inquirer.prompt([
-      {
-        name: 'drive',
-        type: 'list',
-        message: 'Select a drive to mount',
-        choices: drives,
-      },
-    ]);
+/**
+ * mountDrive
+ * @param {string} driveName
+ */
+const mountDrive = async (driveName) => {
+  return exec(`hdiutil attach ${path.join(__testDrives, driveName)}`);
+};
 
-    const spinner = ora(`Mounting ${drive}`).start();
+/***
+ * unmountDrive
+ * @param {string} driveName
+ */
+const unmountDrive = async (driveName) => {
+  const mountInfo = await getMountInfo();
+  const mountedDrive = mountInfo.find((x) => x.name === driveName);
 
-    await exec(`hdiutil attach ${path.join(__testDrives, drive)}`);
+  if (!mountedDrive) {
+    throw new Error(`Drive ${driveName} is not mounted`);
+  }
 
-    spinner.succeed('Drive mounted');
-  });
+  return exec(`hdiutil detach ${mountedDrive.mountPath}`);
+};
 
-program.parse();
+//
+// Main
+//
+const drives = await getDrives();
+
+console.log(
+  table([['Name', 'Mounted?', 'Size'], ...drives.map((x) => Object.values(x))]),
+);
+
+const { action } = await inquirer.prompt([
+  {
+    type: 'list',
+    message: 'What would you like to do?',
+    name: 'action',
+    choices: [
+      { name: 'Create drive', value: 'create' },
+      { name: 'Delete drive', value: 'delete' },
+      { name: 'Mount drive', value: 'mount' },
+    ],
+  },
+]);
+
+if (action === 'create') {
+  //
+  // Create drive
+  //
+
+  /**
+   * @param {number} n
+   * @returns {string}
+   */
+  const getDefault = (n = 1) => {
+    const baseName = 'test-drive';
+    const driveName = `${baseName}-${n}`;
+    if (drives.find((x) => x.name.startsWith(driveName))) {
+      return getDefault(n + 1);
+    }
+    return driveName;
+  };
+
+  const defaultName = getDefault();
+  const { driveName, driveSize } = await inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What would you like to name the drive?',
+      name: 'driveName',
+      default: defaultName,
+    },
+    {
+      type: 'input',
+      message: 'How large would you like the drive to be?',
+      name: 'driveSize',
+      default: defaultName,
+    },
+  ]);
+
+  const _name = `${driveName}.dmg`;
+
+  // Create drive
+  const createSpinner = ora(`Creating drive ${_name}`).start();
+  await exec(
+    `hdiutil create -size 1g -fs 'APFS' -volname ${driveName} ${path.join(__testDrives, _name)}`,
+  );
+  createSpinner.succeed('Drive created');
+
+  // Mount drive
+  const mountSpinner = ora('Mounting drive').start();
+  // await mountDrive();
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  mountSpinner.succeed('Drive mounted');
+} else if (action === 'delete') {
+  //
+  // Delete drive
+  //
+  const { driveName } = await inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What drive would you like to delete?',
+      name: 'driveName',
+      choices: drives.map((x) => x.name),
+    },
+  ]);
+
+  // unount
+} else if (action === 'mount') {
+  //
+  // Mount drive
+  //
+}
