30aaa42f5a75f42357536675a1437a3a41e3c2a8
Kyle Upton <kyleaupton@gmail.com>
2024-09-24 00:05:23 -0400

    refresh disks when `isFinished` changes

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,23 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
+import { storeToRefs } from 'pinia';
 import { useDisksStore, useFlashStore } from '@/stores';
 import Titlebar from '@/components/Titlebar.vue';
 import Main from '@/components/Main.vue';
 
 const disksStore = useDisksStore();
 const flashStore = useFlashStore();
+const { isFinished } = storeToRefs(flashStore);
 
 onMounted(() => {
   disksStore.getDisks();
   disksStore.registerUsbEvents();
   flashStore.registerFlashEvents();
+});
+
+watch(isFinished, () => {
+  disksStore.getDisks();
 });
 </script>
